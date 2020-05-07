af90057911b3a98155feab2b0839949a530459a2
Amal <amt2.vistaprint@gmail.com>
2020-05-08 00:15:53 +0100

    Add Navbar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Navbar from "./globals/Navbar"
 import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return(
     <div>
+      <Navbar/>
       {children}
     </div>
   )
