1fb19f8dd8c2c273520a384bc52b4ed8de5986f4
Amal <amt2.vistaprint@gmail.com>
2020-05-08 16:18:56 +0100

    Add menu component

---
+++ src/components/home/Menu.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react"
+import Title from "../globals/Title"
+import Img from "gatsby-image"
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: props.items.edges,
+      productItems: props.items.edges,
+    }
+  }
+  render() {
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="Best of our menu" />
+
+            <div className="row">
+              {this.state.productItems.map(({ node }) => {
+                return (
+                  <div
+                    key={node.id}
+                    className="col-11 col-md-6 my-3 d-flex mx-auto"
+                  >
+                    <div>
+                      <Img fixed={node.image.fixed} />
+                    </div>
+                    <div className="flex-grow-1 px-3">
+                      <div className="d-flex justify-content-between">
+                        <h6 className="mb-0">
+                          <smal>{node.title}</smal>
+                        </h6>
+                        <h6 className="mb-0 text-yellow">
+                          <small>${node.price}</small>
+                        </h6>
+                      </div>
+                      <p className="text-muted">
+                        <small>{node.description.description}</small>
+                      </p>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="Best of our menu" />
+            <div className="row">
+              <div className="col-10 col-sm-6 mx-auto text-center text-capitalize">
+                <h1>There are no items to display</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { FaItchIo } from "react-icons/fa"
 import { Link, graphql } from "gatsby"
 import BackgroundSection from "../components/globals/BackgroundSection"
 import Info from "../components/home/Info"
+import Menu from "../components/home/Menu"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -15,6 +16,7 @@ const IndexPage = ({ data }) => (
       styleClass="default-background"
     />
     <Info/>
+    <Menu items={data.menu}/>
   </Layout>
 )
 
@@ -24,6 +26,24 @@ export const query = graphql`
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulProductItem {
+      edges {
+        node {
+          id
+          title
+          category
+          price
+          description {
+            description
+          }
+          image {
+            fixed(height: 50, width: 50){
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
