43ce8ca5d0b001a4eed8be0063e325d19dadffdd
Amal <amt2.vistaprint@gmail.com>
2020-05-08 02:11:49 +0100

    Add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,14 +9,16 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Navbar from "./globals/Navbar"
+import Footer from "../components/globals/Footer"
 import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  return(
+  return (
     <div>
-      <Navbar/>
+      <Navbar />
       {children}
+      <Footer />
     </div>
   )
 }
